fe0b12d0ccce81531c1bb35d10eb227bcd8f820b
MrWhat <161951784+MrWhat536@users.noreply.github.com>
2024-09-29 20:48:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var coin = localStorage.getItem('coin');
+var count = 0;
+count = coin;
+var eng = 10000;
+const button = document.getElementById('SyS')
+document.getElementById("yo1").innerHTML =count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "\u202F");
+
+
+function ClickB(el){
+	count ++;
+	document.getElementById("yo1").innerHTML =count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "\u202F");
+	eng--;
+	localStorage.setItem('coin', count);
+
+	
+}
+
